c03211621158cdf8f2094681761f1328bb5a4cd5
Andrew Romanchuk <rainerglol@gmail.com>
2016-01-17 06:15:22 +0200

    test

---
+++ bozon/bozon.js
@@ -0,0 +1,167 @@
+var remote = require('remote');
+var Menu = remote.require('menu');
+var MenuItem = remote.require('menu-item');
+var dialog = remote.require('dialog');
+var fs = require("fs");
+var clipboard = require('clipboard');
+var cw = remote.getCurrentWindow();
+var path = remote.app.getAppPath() + '/experiments/utils';
+var utils = remote.require(path);
+var windowManager = remote.require('electron-window-manager');
+var CodeMirror = require('codemirror');
+
+
+localStorage.Knows = localStorage.Knows || "[]";
+
+var justData = currentKnows();
+
+function currentKnows(data) {
+  var dk = JSON.parse(localStorage.Knows)
+  if (data) {
+    dk[cw.id] = data;
+    localStorage.Knows = JSON.stringify(dk)
+  }
+
+  return dk[cw.id] || {};
+}
+require('electron').ipcRenderer.on('justUpdate', justUpdate);
+require('electron').ipcRenderer.on('justScrollAll', justScrollAll);
+require('electron').ipcRenderer.on('justMoveUpwin', justMoveUpwin);
+require('electron').ipcRenderer.on('justMoveDowin', justMoveDowin);
+require('electron').ipcRenderer.on('justSource', justSource);
+require('electron').ipcRenderer.on('justTest', justTest);
+
+
+function justSource(e, str) {
+  document.getElementById('source').innerHTML = str;
+  return str
+}
+
+function justTest(vt) {
+
+
+}
+
+
+function justRestore(el) {
+  var dk = JSON.parse(localStorage.Knows);
+  cw.webContents.send('justScrollAll');
+  function startKnows(know, id) {
+    if (id < 2) return;
+
+    windowManager.open(null, 'Editor', 'file://' + __dirname + '/index.html');
+    var win = remote.BrowserWindow.fromId(id);
+
+    if (win) {
+
+      win.webContents.on("did-finish-load", function () {
+        win.webContents.send('justUpdate');
+        win.webContents.send('justScrollAll');
+      })
+    }
+  }
+
+  dk.forEach(startKnows)
+
+
+}
+
+function justUpdate(el) {
+
+  var ed = editor.getValue();
+
+  if (ed.trim()) {
+    justData.sourceText = ed;
+  }
+
+  editor.setValue(currentKnows(justData).sourceText);
+}
+
+function justMoveUpwin(e, db) { // from down window
+  var cb = cw.getBounds();
+  cw.setPosition(db.x, db.y - cb.height);
+
+  cb = cw.getBounds();
+  var uw = windowManager.get('bozon_'+(cw.id - 1)).content();
+  if (uw)
+    uw.send('justMoveUpwin', cb);
+}
+
+function justMoveDowin(e, ub) { // from upper window
+  var cb = cw.getBounds();
+  cw.setPosition(ub.x, ub.y + ub.height);
+
+  cb = cw.getBounds();
+  var dw = windowManager.get('bozon_'+(cw.id + 1)).content();
+  if (dw)
+    dw.send('justMoveDowin', cb);
+
+}
+
+function justScrollAll(xy) {
+
+  document.body.onmousewheel = function (me) {
+    var cb = cw.getBounds();
+    cb.y += me.wheelDeltaY / 3 | 0;
+
+    cw.setPosition(cb.x, cb.y)
+
+    var uw = windowManager.get('bozon_'+(cw.id - 1)).content();
+
+    if (uw)
+      uw.send('justMoveUpwin', cb);
+
+    var dw = windowManager.get('bozon_'+(cw.id + 1)).content();
+    if (dw)
+      dw.send('justMoveDowin', cb);
+
+
+  }
+}
+
+
+var justResultWindow = false;
+function runFile(el) {
+  el.classList.add('btn-warning');
+
+  //handleSaveButton()
+  if (fileEntry) {
+
+    if (justResultWindow && !justResultWindow.closed) {
+      remote.BrowserWindow.fromId(justResultWindow.guestId).reloadIgnoringCache();
+    } else {
+      justResultWindow = utils.newTransparentWindow('file://' + fileEntry);
+    }
+  } else {
+    justResultWindow = utils.newTransparentWindow(utils.genDataHtmlUrl(editor.getValue()))
+  }
+}
+
+onload = function () {
+  document.body.onmousewheel = function (me) {
+    var p = cw.getPosition();
+    var sp = me.wheelDeltaY / 3 | 0;
+    // console.log(sp, test+=sp)
+    cw.setPosition(p[0], p[1] + sp);
+    return false
+  }
+
+  syncSizeLines();
+};
+
+onresize = function () {
+};
+
+syncSizeLines = function () {
+  var results = document.getElementById('source');
+  var containerWidth = 680;// container.offsetWidth;
+  var resultsHeight = results.offsetHeight;
+
+
+  var height = 0;
+
+  remote.getCurrentWindow().setSize(containerWidth, height + resultsHeight);
+
+}
+
+
--- main.js
+++ main.js
@@ -3,47 +3,52 @@ const BrowserWindow = require('browser-window');
 var windowManager = require('electron-window-manager');
 
 
-//
-//app.on('window-all-closed', function () {
-//  // if (process.platform != 'darwin')
-//  app.quit();
-//});
+app.on('window-all-closed', function () {
+  // if (process.platform != 'darwin')
+  app.quit();
+});
+
+
+app.on('before-quit', function () {
+  app.quit();
+});
 
 app.on('ready', function () {
   windowManager.init({
-    //'layouts': {
-    //  'default': '/layouts/default.html',  // The "/" at the start will be replaced with the 'appBase' value
-    //  'secondary': '/layouts/secondary.html'
-    //},
-
-    'devMode': false
-  });
-
-  windowManager.setDefaultSetup({
-    position: 'center',
-    width: 680,
-    height: 240,
-    //x:650,y:420,
-    transparent: true,
-    acceptFirstMouse: true,
-    enableLargerThanScreen: true,
-    alwaysOnTop: false,
-    webSecurity: false,
-    zoomFactor: 1,
-    titleBarStyle: 'hidden',
-    show: true,
-    resizable: true,
-    plugins: true,
-    frame: true,
-    "web-preferences": {
-      "experimental-features": true,
-      "experimental-canvas-features": true,
-      "subpixel-font-scaling": true,
-      "overlay-scrollbars": true
+    'appBase': __dirname,
+    "onLoadFailure": function (window) {
+      //window.content().loadURL('file://' + __dirname + '/loadFailure.html');
+    },
+    'devMode': false,
+    'defaultSetup': {
+      //position: 'center',
+      width: 680,
+      height: 240,
+      //x:650,y:420,
+      transparent: true,
+      acceptFirstMouse: true,
+      enableLargerThanScreen: true,
+      alwaysOnTop: false,
+      webSecurity: false,
+      zoomFactor: 1,
+      titleBarStyle: 'hidden',
+      show: true,
+      resizable: true,
+      plugins: true,
+      frame: true,
+      "web-preferences": {
+        "experimental-features": true,
+        "experimental-canvas-features": true,
+        "subpixel-font-scaling": true,
+        "overlay-scrollbars": true
+      }
     }
   });
 
-  windowManager.open('editor', 'Editor', 'file://' + __dirname + '/editor/index.html');
-  windowManager.open('bozon', 'Bozon', 'file://' + __dirname + '/bozon/index.html');
+  we = windowManager.createNew('Editor', 'Editor', 'file://' + __dirname + '/editor/index.html');
+  we.open();
+  we.move('right');
+  we.execute('justUpdate()');
+  windowManager.createNew('bozon', 'Bozon', 'file://' + __dirname + '/bozon/index.html').open();
 
 });
